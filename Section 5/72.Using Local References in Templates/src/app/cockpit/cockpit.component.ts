f5ac2e18cf2a02de23ad453d3257bf3f38187672
AELCharis <AELCharis>
2019-08-18 14:35:59 +0300

    72.Using Local References in Template, (Pos vazo ena reference sto <input> k to pernao sto button pou kali ti function)

---
+++ Section 5/72.Using Local References in Templates/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,37 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+ @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string }>(); //outpu gia na peraso ta stixia ekso apo to component
+  //EventEmitter gia custome events k to kano import apo to angular core
+ @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string }>();
+
+  // newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+onAddServer(nameInput: HTMLInputElement) {
+  this.serverCreated.emit({
+    serverName: nameInput.value,
+    serverContent: this.newServerContent
+  });
+}
+
+onAddBlueprint(nameInput: HTMLInputElement) {
+  this.blueprintCreated.emit({
+    serverName: nameInput.value,
+    serverContent: this.newServerContent
+  });
+}
+
+
+
+}
